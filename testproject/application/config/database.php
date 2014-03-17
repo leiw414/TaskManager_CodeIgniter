1d9e9bf5ad7b5d9d1a001778fe78be183e27e463
leiw414 <leiw414@gmail.com>
2014-03-17 00:06:22 -0400

    Update database.php

--- testproject/application/config/database.php
+++ testproject/application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'testproject';
+$db['default']['hostname'] = '127.13.31.130:3306';
+$db['default']['username'] = 'adminjfmTMvs';
+$db['default']['password'] = 'qJwLNExXPuiH';
+$db['default']['database'] = 'codeigniterproject';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
